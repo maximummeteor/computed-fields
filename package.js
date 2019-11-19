8cf3c5f880b27f22635f4388c1093ff71be31eaa
Max Nowack <max@unsou.de>
2019-11-19 18:24:05 +0100

    v2.0.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'maximum:computed-fields',
   summary: 'Meteor package for automatic computation of field values in a collection',
-  version: '1.0.0',
+  version: '2.0.0',
   git: 'https://github.com/maximummeteor/computed-fields.git'
 });
 
@@ -10,14 +10,14 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.6.1');
+  api.versionsFrom('1.7.0.5');
   api.use([
     'coffeescript@2.0.0',
     'underscore',
     'check',
     'modules',
     'lai:collection-extensions@0.2.1_1',
-    'matb33:collection-hooks@0.8.1',
+    'matb33:collection-hooks@1.0.0',
     'tmeasday:check-npm-versions@0.3.2',
   ], 'server');
   api.use([
